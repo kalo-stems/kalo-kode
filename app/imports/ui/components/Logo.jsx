9ed3eacfb130d1affc4b469bc94bee3c6957c9ef
Samantha Kocher <kochers@hawaii.edu>
2024-04-05 22:37:43 -1000

    Take 1
    Changed the Navbar to include Kalo Stems Logo, added navbar options (Our Mission, About Us), started formatting in style.css.

---
+++ app/imports/ui/components/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Image, Nav, Navbar } from 'react-bootstrap';
+
+const Logo = () => (
+  <Navbar expand="lg" bg="light">
+    <Container className="py-2">
+      <Nav className="mx-auto">
+        <Nav.Link>
+          <Image src="/images/kalo-stems-logo.png" fluid className="d-block mx-auto" />
+        </Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+);
+
+export default Logo;
