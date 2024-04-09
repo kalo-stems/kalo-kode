5ef80a4586dd842cf6ace43997f74c8f2660db6f
Natalie Dang <dang2@hawaii.edu>
2024-04-09 11:30:26 -1000

    Update student.js

--- app/imports/api/student/student.js
+++ app/imports/api/student/student.js
@@ -15,6 +15,51 @@ class StudentInfo {
     this.github = '';
   }
 
+  // Setters for updating information
+  setPhone(phone) {
+    this.phone = phone;
+  }
+
+  setEmail(email) {
+    this.email = email;
+  }
+
+  setGraduationDate(date) {
+    this.graduationDate = date;
+  }
+
+  addAchievement(achievement) {
+    this.achievements.push(achievement);
+  }
+
+  setLinkedIn(link) {
+    this.linkedIn = link;
+  }
+
+  setBio(bio) {
+    this.bio = bio;
+  }
+
+  setMajor(major) {
+    this.major = major;
+  }
+
+  addSkill(skill) {
+    this.skills.push(skill);
+  }
+
+  addAward(award) {
+    this.awards.push(award);
+  }
+
+  setDOB(dob) {
+    this.dob = dob;
+  }
+
+  setGitHub(github) {
+    this.github = github;
+  }
+}
 
 // Export the class for use in other modules
 export default StudentInfo;
