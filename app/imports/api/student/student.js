4f5f98caae729a7ac2e612d83b16922177d85bd2
Natalie Dang <dang2@hawaii.edu>
2024-04-09 11:20:46 -1000

    not done

---
+++ app/imports/api/student/student.js
@@ -0,0 +1,20 @@
+import { Selector } from 'testcafe';
+
+class StudentInfo {
+  constructor() {
+    this.phone = '';
+    this.email = '';
+    this.graduationDate = '';
+    this.achievements = [];
+    this.linkedIn = '';
+    this.bio = '';
+    this.major = '';
+    this.skills = [];
+    this.awards = [];
+    this.dob = '';
+    this.github = '';
+  }
+
+
+// Export the class for use in other modules
+export default StudentInfo;
