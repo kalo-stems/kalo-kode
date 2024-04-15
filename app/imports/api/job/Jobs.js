3b1b9cefa3e70f624a87f9382752258d0de44a0a
Natalie Dang <dang2@hawaii.edu>
2024-04-15 10:59:47 -1000

    not done

--- app/imports/api/job/Jobs.js
+++ app/imports/api/job/Jobs.js
@@ -1,16 +1,10 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 
-/**
- * The JobsCollection. It encapsulates state and variable values for job.
- */
 class JobsCollection {
   constructor() {
-    // The name of this collection.
     this.name = 'JobsCollection';
-    // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
-    // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
       title: String,
       description: String,
@@ -19,17 +13,26 @@ class JobsCollection {
       company: String,
       deadline: String,
       seats: Number,
+      acceptedApplications: Number,
+      steps: Array, // Array to store steps
+      'steps.$': String, // Each step is a string
     });
-    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
-    // Define names for publications and subscriptions
     this.userPublicationName = `${this.name}.publication.user`;
     this.adminPublicationName = `${this.name}.publication.admin`;
   }
+
+  // Method to update the list of steps for a job
+  updateSteps(jobId, steps) {
+    this.collection.update(jobId, { $set: { steps: steps } });
+  }
+
+  // Method to get the list of steps for a job
+  getSteps(jobId) {
+    const job = this.collection.findOne(jobId);
+    return job ? job.steps : [];
+  }
 }
 
-/**
- * The singleton instance of the JobsCollection.
- * @type {JobsCollection}
- */
 export const Jobs = new JobsCollection();
+
