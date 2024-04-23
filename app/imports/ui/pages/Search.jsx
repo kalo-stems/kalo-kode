061791c9fa60786625e9ad352a7624920ff9df07
Samantha Kocher <kochers@hawaii.edu>
2024-04-23 11:46:42 -1000

    Update Search.jsx

--- app/imports/ui/pages/Search.jsx
+++ app/imports/ui/pages/Search.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const Search = () => (
+const SearchList = () => (
   <>
     <input id="search-bar" className="fluid" size="50" type="text" placeholder="Search" />
     <Button className="px-2" id="search-button">Search</Button>
   </>
 );
 
-export default Search;
+export default SearchList;
