08eaabe7baeab892278e888299b360541acf34db
jairabp <156969453+jairabp@users.noreply.github.com>
2024-04-23 11:37:37 -1000

    Debugged Issue 23

--- app/imports/ui/pages/ListCompany.jsx
+++ app/imports/ui/pages/ListCompany.jsx
@@ -6,7 +6,7 @@ import { Companies } from '../../api/company/Company';
 import CompanyItem from '../components/CompanyItem';
 import LoadingSpinner from '../components/LoadingSpinner';
 
-/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+/* Renders a table containing all the Company documents. Use <CompanyItem> to render each row. */
 const ListCompany = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, companies } = useTracker(() => {
