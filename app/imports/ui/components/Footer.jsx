d0bb6863b26f3313526a722d98191be766e77b8a
jairabp <156969453+jairabp@users.noreply.github.com>
2024-05-07 19:35:46 -1000

    updated issue 22 Footer.jsx

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -8,15 +8,24 @@ const Footer = () => (
       <Col className="text-center">
         Natalie Dang, Emily Hsu, Samantha Kocher, Roma Malasarte, Rina Ogino, Jaira Pader
         <br />
+        ndang562@hawaii.edu, ehsu@hawaii.edu, kochers@hawaii.edu, romaamor@hawaii.edu, rin4@hawaii.edu, jairabp@hawaii.edu
+        <br />
+        <br />
         Department of Information and Computer Sciences
         {' '}
         <br />
         University of Hawaii
         <br />
-        ndang562@hawaii.edu, ehsu@hawaii.edu, kochers@hawaii.edu, romaamor@hawaii.edu, rin4@hawaii.edu, jairabp@hawaii.edu
+        Honolulu, HI 96822
         {' '}
         <br />
-        <a style={{ color: 'green' }} href="https://kalo-stems.github.io/">https://kalo-stems.github.io/</a>
+        <br />
+        <a style={{ color: 'white' }} href="https://ics-software-engineering.github.io/meteor-application-template-react/">Template Home Page</a>
+        <br />
+        <a style={{ color: 'white' }} href="https://kalo-stems.github.io/">https://kalo-stems.github.io/</a>
+        <br />
+        <br />
+        <a style={{ color: 'white' }} href="https://docs.google.com/forms/d/e/1FAIpQLSe2SBh4m5ZspDTEs6nUZX3oFsPsw_8W-QFEuCeRV7GaNbes3g/viewform?usp=pp_url">Feedback Form</a>
       </Col>
     </Container>
   </footer>
