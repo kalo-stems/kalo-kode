d42628bd607ae43e26f186fde8d7e4f05a89ab60
jairabp <156969453+jairabp@users.noreply.github.com>
2024-05-08 14:30:26 -1000

    issue 25

--- app/imports/ui/pages/AboutUs.jsx
+++ app/imports/ui/pages/AboutUs.jsx
@@ -1,34 +1,57 @@
 import React from 'react';
-import { Image, Col, Row } from 'react-bootstrap';
+import { Image, Col, Row, Container } from 'react-bootstrap';
 
 /* A simple static component to render some text for the about us page. */
 const AboutUs = () => (
-  <Row id="aboutus-page" fluid>
-    <Image src="images/background.png" id="background" width="cover" />
-    <Col xs={8} className="text-center position-absolute top-50 start-50 translate-middle" id="body-text">
-      <Row className="align-content-center justify-content-center">
-        <h1><strong>Welcome to Kalo Stems!</strong></h1>
-        <h2 id="motto">&quot;Cultivate Your Future, Stem Your Network with Companies!&quot;</h2>
-        {/* eslint-disable-next-line max-len */}
-        <h4>Our platform, Kalo Stems, will allow UH students to connect with career opportunities that are tailored to their interests and skills. Students will be able to specify their background and aspirations, allowing companies to filter and match students who are best fit for their opportunity. KaloStems bridges the gap between academia and industry, allowing students long-term professional success.
-        </h4>
-        <h1> </h1>
-        {/* eslint-disable-next-line max-len */}
-        <h4>Kalo Stems is based upon meteor-application-template-react and meteor-example-form-react. Please use the videos and documentation at those sites to better acquaint yourself with the basic application design and form processing in Kalo-Stems.
-        </h4>
-      </Row>
-    </Col>
+  <Container className="text-center" id="landing-page" fluid="true">
+    <br />
+    <Row>
+      <h1><strong> Welcome to Kalo Stems! </strong></h1>
+      <h2 id="motto">&quot;Cultivate Your Future, Stem Your Network with Companies!&quot;</h2>
+      {/* eslint-disable-next-line max-len */}
+      <h4>Our platform, Kalo Stems, will allow UH students to connect with career opportunities that are tailored to their interests and skills. Students will be able to specify their background and aspirations, allowing companies to filter
+        and match students who are best fit for their opportunity. Kalo Stems bridges the gap between academia and industry, allowing students long-term professional success.
+      </h4>
+    </Row>
+    <br />
+    <br />
+    <Row />
+    <Row><h1><strong> The Creators </strong></h1></Row>
     <Row>
       <Col>
-        <h1>The Creators</h1>
+        <Image id="JairaPader" src="images/JairaPader.jpg" width="300" />
+        <h3>Jaira Pader</h3>
+      </Col>
+      <Col>
+        <Image id="EmilyHsu" src="images/EmilyHsu.png" width="300" />
+        <h3>Emily Hsu</h3>
+      </Col>
+      <Col>
+        <Image id="SamanthaKocher" src="images/SamanthaKocher.jpg" width="300" />
+        <h3>Samantha Kocher</h3>
       </Col>
     </Row>
-    <Row>Natalie Dang</Row>
-    <Row>Emily Hsu</Row>
-    <Row>Samantha Kocher</Row>
-    <Row>Roma Malasarte</Row>
-    <Row>Rina Ogino</Row>
-    <Row>Jaira Pader</Row>
-  </Row>
+    <br />
+    <br />
+    <Row>
+      <Col>
+        <Image id="RinaOgino" src="images/RinaOgino.png" width="300" />
+        <h3>Rina Ogino</h3>
+      </Col>
+      <Col>
+        <Image id="RomaMalasarte" src="images/RomaMalasarte.jpg" width="300" />
+        <h3>Roma Malasarte</h3>
+      </Col>
+      <Col>
+        <Image id="NatalieDang" src="images/NatalieDang.jpeg" width="300" />
+        <h3>Natalie Dang</h3>
+      </Col>
+    </Row>
+    <Row>
+      {/* eslint-disable-next-line max-len */}
+      <h6>Kalo Stems is based upon meteor-application-template-react and meteor-example-form-react. Please use the videos and documentation at those sites to better acquaint yourself with the basic application design and form processing in Kalo Stems.
+      </h6>
+    </Row>
+  </Container>
 );
 export default AboutUs;
