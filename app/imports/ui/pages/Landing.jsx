8b6ad8a863da161918b7fb72c46ea624eca3dd92
jairabp <156969453+jairabp@users.noreply.github.com>
2024-05-08 16:11:22 -1000

    issue 29

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -10,17 +10,25 @@ const Landing = () => (
         <h1><strong>Welcome to Kalo Stems!</strong></h1>
         <h2 id="motto">&quot;Cultivate Your Future, Stem Your Network with Companies.&quot;</h2>
       </Row>
+      <br />
+      <br />
       <Row>
         <Col>
           <Image id="sample-image-1" src="images/addCompanyProfile.png" />
+          <br />
+          <br />
           <h5>Add your company&apos;s profile to our application for students to browse.</h5>
         </Col>
         <Col>
           <Image id="sample-image-2" src="images/listJobs.png" />
+          <br />
+          <br />
           <h5>Students may browse all job listings posted by the companies.</h5>
         </Col>
         <Col>
           <Image id="sample-image-3" src="images/listCompanyProfile.png" />
+          <br />
+          <br />
           <h5>Students may also browse a complete list of all of the companies in our database!</h5>
         </Col>
       </Row>
